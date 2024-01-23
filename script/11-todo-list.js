1743515841a14fc9c723af04cf6536febb0a296f
Chingthangkhomba <you@example.com>
2024-01-23 08:21:33 +0530

    display-todolist

--- script/11-todo-list.js
+++ script/11-todo-list.js
@@ -1,9 +1,27 @@
-const todoList = [];
+const todoList = [''];
+renderTodoList();
+function renderTodoList (){
+let todoListHTML = '';
+for (let i = 0; i <= todoList.length - 1; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+}
+
+document.querySelector('.js-input-list').innerHTML = 
+todoListHTML;}
+
+function handleListKeyDown(event){
+    if (event.key === 'Enter'){
+   addTodo();
+ }
+    }
+
+
 function addTodo(){
     const inputElement = document.querySelector('.js-input-name');
     const name = inputElement.value;
     todoList.push(name);
-    console.log(todoList);
     inputElement.value = '';
-
+    renderTodoList();
 }
